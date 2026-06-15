03e77ec10be786f327934b5b126668d404d393f6
OmThakur-123 <vatsom2007@gmail.com>
2026-06-15 21:10:48 +0530

    Index.js upadte

--- Backend Swiggy/backend/index.js
+++ Backend Swiggy/backend/index.js
@@ -21,6 +21,7 @@ app.get("/top-restaurant-chains", (req, res) => {
   res.json(restaurantChains);
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
